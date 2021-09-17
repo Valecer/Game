f8ee2f5624639ad3a845cf834f9d475acf922dbd
Valecer <mark.borisoff2017@yandex.ru>
2021-09-18 05:26:37 +0700

    'easyfix'

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from '../../assets/img/logo.png'
 const Navbar = () => {
     return (
         <div className="header">
-            <h1 className="logo"><a href="#"><img src={Logo} alt="" className="im"/></a></h1>
+            <h1 className="logo"><a href="#"><img src={Logo} alt="" className=""/></a></h1>
             <ul className="main-nav">
                 <li><a href="#">Правила</a></li>
                 <li><a href="#">Войти</a></li>
